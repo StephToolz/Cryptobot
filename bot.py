a90f41972aeade8c631d1eab27b06d3585f2817d
Darkness <159263983+StephToolz@users.noreply.github.com>
2026-09-17 12:20:02 +0200

    Update environment variable names for Telegram bot

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import os
 import requests
 
 # Načtení tokenu z proměnných prostředí (bezpečně)
-TELEGRAM_TOKEN = os.getenv("8881136619:AAG7JPMIL6ZG1g1Gtko9sH0GP00hcf-icqk")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 # Příklad použití ID chatu, kam má zpráva dorazit (také může být v env proměnných)
-CHAT_ID = os.getenv("7260268357")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def get_crypto_price():
     """Získá aktuální cenu BTC z veřejného CoinGecko API"""
